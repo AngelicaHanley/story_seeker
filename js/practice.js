d97251a0403260296e20fb279f5b161dc7afbee5
angelicahanley <hanleyangelica@ufl.edu>
2024-07-31 03:31:21 -0400

    Created array of selected books and stores title

--- js/practice.js
+++ js/practice.js
@@ -5,7 +5,7 @@ let d = new Date();
 document.body.innerHTML = "<h0>Today's date is " + d + "</h0>"
 //alert("Hi, welcome to Story Seeker!"); //sends an alert message on the screen right when opened
 //potentially could do like if count == 12 then put alert saying bookshelf full!
-let name = prompt("Welcome to Story Seeker! What is your name?"); //sends message on screen when opened, asking for user's name
+//let name = prompt("Welcome to Story Seeker! What is your name?"); //sends message on screen when opened, asking for user's name
 //later prob see if after home page, if when user presses "start" button, we could have that pop up
 //alert("Hello, " + name + "!");
 console.log("Hello, World!");
--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
+
 // Book constructor function
 function Book(title, imagePath, genres) {
     this.title = title;
-    this.imagePath = imagePath; // Path to the book cover image
+    this.imagePath = imagePath;
     this.bookClicked = false;
     this.frameVisible = false;
     this.genres = genres;
@@ -13,24 +14,53 @@ let books = [
         new Book("We Were Liars", "css/images/WeWereLiarsCover.jpeg", ["Mystery", "Young Adult"]),
         new Book("The Great Gatsby", "css/images/TheGreatGatsbyCover.jpeg", ["Classic", "Fiction"]),
         new Book("The Summer I Turned Pretty", "css/images/TheSummerITurnedPrettyCover.jpg", ["Romance", "Young Adult"]),
-        new Book("Fourth Wing", "css/images/FourthWingCover.jpeg", ["Fantasy", "Adventure"]),
-        new Book("Harry Potter", "css/images/HarryPotterCover.jpeg", ["Fantasy", "Adventure"]),
+        new Book("Fourth Wing", "css/images/FourthWingCover.jpeg", ["Fantasy", "Romance"]),
+        new Book("Harry Potter", "css/images/HarryPotterCover.jpeg", ["Fantasy", "Fiction"]),
         new Book("All The Bright Places", "css/images/AllTheBrightPlacesCover.jpeg", ["Romance", "Young Adult"]),
-    new Book("The Selection", "css/images/TheSelectionCover.jpg", ["Romance", "Fiction"]),
+        new Book("The Selection", "css/images/TheSelectionCover.jpg", ["Romance", "Young Adult"]),
         new Book("Heartstopper", "css/images/HeartstopperCover.jpeg", ["Romance", "Graphic Novel"]),
-        new Book("The Seven Husbands of Evelyn Hugo", "css/images/TheSevenHusbandsOfEvelynHugoCover.jpeg", ["Historical", "Romance"]),
+        new Book("The Seven Husbands of Evelyn Hugo", "css/images/TheSevenHusbandsOfEvelynHugoCover.jpeg", ["Historical", "Contemporary"]),
         new Book("Today Tonight Tomorrow", "css/images/TodayTonightTomorrowCover.jpeg", ["Romance", "Young Adult"]),
-        new Book("The Cruel Prince", "css/images/TheCruelPrinceCover.jpeg", ["Fantasy", "Adventure"]),
+        new Book("The Cruel Prince", "css/images/TheCruelPrinceCover.jpeg", ["Fantasy", "Young Adult"]),
 
-        new Book("Before We Were Strangers", "css/images/BeforeWeWereStrangersCover.jpeg", ["Romance", "Fiction"]),
-        new Book("Better Than The Movies", "css/images/BetterThanTheMoviesCover.jpeg", ["Mystery", "Young Adult"]),
-        new Book("Shatter Me", "css/images/ShatterMeCover.jpeg", ["Classic", "Fiction"]),
-    new Book("The Naturals", "css/images/TheNaturalsCover.jpg", ["Romance", "Young Adult"]),
-    new Book("Happy Place", "css/images/HappyPlaceCover.jpg", ["Fantasy", "Adventure"]),
-    new Book("The Lightning Thief", "css/images/TheLightningThiefCover.jpg", ["Fantasy", "Adventure"]),
-    new Book("A Court of Thorns and Roses", "css/images/ACourtOfThornsAndRosesCover.jpg", ["Romance", "Young Adult"]),
-    new Book("The Outsiders", "css/images/TheOutsidersCover.jpg", ["Romance", "Fiction"])
+        new Book("Before We Were Strangers", "css/images/BeforeWeWereStrangersCover.jpeg", ["Romance", "Contemporary"]),
+        new Book("Better Than The Movies", "css/images/BetterThanTheMoviesCover.jpeg", ["Romance", "Young Adult"]),
+        new Book("Shatter Me", "css/images/ShatterMeCover.jpeg", ["Dystopia", "Young Adult"]),
+        new Book("The Naturals", "css/images/TheNaturalsCover.jpg", ["Mystery", "Thriller"]),
+        new Book("Happy Place", "css/images/HappyPlaceCover.jpg", ["Romance", "Contemporary"]),
+        new Book("The Lightning Thief", "css/images/TheLightningThiefCover.jpg", ["Fantasy", "Young Adult"]),
+        new Book("A Court of Thorns and Roses", "css/images/ACourtOfThornsAndRosesCover.jpg", ["Fantasy", "New Adult"]),
+        new Book("The Outsiders", "css/images/TheOutsidersCover.jpg", ["Classic", "Fiction"])
 ];
+
+let selectedBooks = [];
+
+// Function to handle book click
+function handleBookClick(book, img) {
+    // Check if the book is already in the selectedBooks array
+    const index = selectedBooks.indexOf(book);
+
+    if (index === -1) {
+        // Book is not in the array, add it
+        selectedBooks.push(book);
+        // Add box shadow to the book image when clicked
+        img.style.boxShadow = '0 0 15px 12px lightskyblue';
+    } else {
+        // Book is in the array, remove it
+        selectedBooks.splice(index, 1);
+        // Remove box shadow from the book image when clicked again
+        img.style.boxShadow = 'none';
+    }
+
+    // Optionally log the list of selected books to the console for debugging
+   // console.log('Selected Books:', selectedBooks);
+    // Log the titles of selected books to the console
+    let selectedTitles = [];
+    for (const selectedBook of selectedBooks) {
+        selectedTitles.push(selectedBook.title);
+    }
+    console.log('Selected Books:', selectedTitles);
+}
 
 // Function to display books
 function displayBooks() {
@@ -69,12 +99,13 @@ function displayBooks() {
  
          // Append image and title to the book container
          bookDiv.appendChild(img);
-        // bookDiv.appendChild(title);
+         bookDiv.appendChild(title);
  
          // Append the book container to the grid
          bookGrid.appendChild(bookDiv);
 
 
+/*
         // Add click event listener to the book div
         bookDiv.addEventListener('click', () => {
             // Toggle bookClicked state
@@ -85,15 +116,20 @@ function displayBooks() {
                // img.style.border = '10px solid lightskyblue'; // Example highlight
                img.style.boxShadow = '0 0 15px 12px lightskyblue';
             } else {
-                img.style.border = 'none';
+                img.style.boxShadow  = 'none';
             }
-        });
+        }); */
 
         // Append image to the book container
         bookDiv.appendChild(img);
 
         // Append the book container to the grid
         bookGrid.appendChild(bookDiv);
+
+        // Add click event listener to the book div
+        bookDiv.addEventListener('click', () => {
+            handleBookClick(book, img);
+        });
     });
 }
 
